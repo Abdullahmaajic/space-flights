65f4908fcf214ec114dc48680a3c2a13c0da08bf
Abdullahmaajic <abdullahmaajic@gmail.com>
2022-01-24 14:39:00 +0530

    changes-updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpComponent } from './emp/emp.component';
+import { FlightComponent } from './flight/flight.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:FlightComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightComponent } from './flight/flight.component';
+import { EmpComponent } from './emp/emp.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightComponent,
+    EmpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
